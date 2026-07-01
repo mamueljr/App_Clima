2e72c770df942980649517f15a8b67efb0325292
Emmanuel Rojas <mamueljr@gmail.com>
2026-06-30 23:00:22 -0600

    fix: corregir espaciado de cabeceras de tarjetas y distribucion de columnas en pronostico de 7 dias para movil

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * AuraWeather Service Worker for PWA
  */
 
-const CACHE_NAME = 'aura-weather-cache-v10';
+const CACHE_NAME = 'aura-weather-cache-v11';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
